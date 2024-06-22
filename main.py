6c43dd09ee9b7b89ddc2f369a5d590cd36e15f02
Estebamq <quiroz_estebanmarcelo@live.com>
2024-06-22 01:04:09 -0300

    nueva interfase

--- main.py
+++ main.py
@@ -1,77 +1,94 @@
-import flet as ft
+import flet
+from flet import *
+from math import pi
+import time
 import subprocess
 
 
-def main(page: ft.Page):
+class AnimatedBox(UserControl):
+    def __init__(self, border_color, bg_color, rotate_angle):
+        self.border_color = border_color
+        self.bg_color = bg_color
+        self.rotate_angle = rotate_angle
+        super().__init__()
 
-    #propiedades de la pagina
-    page.title = "Password Secret App"
+    def build(self):
+        return Container(
+            width=48,
+            height=48,
+            border=border.all(2.5, self.border_color),
+            bgcolor=self.bg_color,
+            border_radius=2,
+            rotate=transform.Rotate(self.rotate_angle, alignment.center),
+            animate_rotation=animation.Animation(700, "easeInOut"),
+        )
+
+
+def main(page: Page):
     page.window_center()
-    page.window_height=800
-    page.window_width=600
-    
-    
-    
-    dlg = ft.AlertDialog(
-        title=ft.Text(""),
-        content=ft.Text("")
-    )
+    page.horizontal_alignment = "center"
+    page.vertical_alignment = "center"
+    page.padding = padding.only(right=50)
+    page.bgcolor = "#0B2D48"
 
-    
-    password = ft.TextField(password=True,hint_text="Ingrese la Contraseña")
 
-    text_custom = ft.Text(  
-                            spans=[
-                                
-                                ft.TextSpan(
-                                    "Rio Labs",
-                                    
-                                    ft.TextStyle(
-                                        size=40,
-                                        weight=ft.FontWeight.BOLD,
-                                        foreground=ft.Paint(
-                                            gradient=ft.PaintLinearGradient(
-                                                (0, 20), (150, 20), [ft.colors.BLUE, ft.colors.GREEN]
-                                            )
-                                        ),
-                                    ),
-                                ),
-                            ],
-                            text_align = ft.TextAlign.CENTER,
-                            
+    intentos = Ref
+    intentos.current = 3
+    
+    password = TextField(
+                        password=True,
+                        hint_text="Ingrese la Contraseña",
+                        text_align=TextAlign.CENTER, 
+                        border_radius=5,
+                        height=100,
+                        width=350,
+                        border_color=colors.WHITE
                         )
 
+    dlg = AlertDialog(
+        title=Text(""),
+        content=Text("")
+    )
+
+        
+    
     def close_dlg(e):
         dlg_modal.open = False
         page.update()
 
     def open_dlg(e):
         page.dialog = dlg
-        if password.value != "hola":
+
+        if password.value != "1117":
+            if intentos.current > 0:
+                intentos.current -= 1
             dlg.open = True
             seconds = 5
-            dlg.title = ft.Text("Contraseña incorrecta")
-            dlg.content = ft.Text("por seguridad el sistema se reiniciara en %d segundos." % seconds)
-            dlg.icon = ft.Icon(ft.icons.NO_ENCRYPTION_GMAILERRORRED, color=ft.colors.RED, size=40)
-            #subprocess.run("shutdown -r -t %d" % seconds)
+            dlg.title = Text("Contraseña incorrecta")
+            dlg.icon = Icon(icons.NO_ENCRYPTION_GMAILERRORRED, color=colors.RED, size=40)
+            if intentos.current == 0:
+                dlg.content = Text("por seguridad el sistema se reiniciara en %d segundos." % seconds)
+                subprocess.run("shutdown -r -t %d" % seconds)
+            else:
+                dlg.content = Text(f"Cantidad de intentos restantes: {intentos.current}")
         else:
             dlg.open = True
-            dlg.title = ft.Text("Contraseña correcta")
+            dlg.title = Text("Contraseña correcta")
 
-            dlg.content = ft.Text("El mensaje secreto es: ")
-            dlg.icon = ft.Icon(ft.icons.LOCK_OPEN_OUTLINED, color=ft.colors.GREEN, size=40)
+            dlg.content = Text("El mensaje secreto es: ")
+            dlg.icon = Icon(icons.LOCK_OPEN_OUTLINED, color=colors.GREEN, size=40)
         password.value = ""
         page.update()
 
 
-    dlg_modal = ft.AlertDialog(
+    dlg_modal = AlertDialog(
         modal=True,
-        title=ft.Text("Info"),
-        content=ft.Text("tendras tres intentos para resolver, luego se reiniciara la pc"),
+        title=Text("Información"),
+        content=Text("VaultChest es un programa diseñado para guardar de manera segura las contraseñas de laboratorio.\nLos usuarios pueden intentar ingresar la contraseña hasta tres veces.\nSi se equivocan más de tres veces, por motivos de seguridad, la computadora se reiniciará y deberán probar nuevamente."),
         actions=[
-            ft.TextButton("cerrar", on_click=close_dlg),
+            TextButton("cerrar", on_click=close_dlg),
         ],
-        actions_alignment=ft.MainAxisAlignment.END
+        actions_alignment = MainAxisAlignment.END
     )
 
     def open_dlg_modal(e):
@@ -79,21 +96,110 @@ def main(page: ft.Page):
         dlg_modal.open = True
         page.update()
 
-    
+    def animate_boxes():
+        clock_wise_rotate = pi / 4
+        counter_clock_wise_rotate = -pi * 2
+        red_box = page.controls[0].content.content.controls[1].controls[0].controls[0]
+        blue_box = page.controls[0].content.content.controls[1].controls[1].controls[0]
+        counter = 0
+        while True:
+            if counter >= 0 and counter <= 4:
+                red_box.rotate = transform.Rotate(
+                    counter_clock_wise_rotate, alignment.center
+                )
+                blue_box.rotate = transform.Rotate(clock_wise_rotate, alignment.center)
+
+                red_box.update()
+                blue_box.update()
+
+                clock_wise_rotate += pi / 2
+                counter_clock_wise_rotate -= pi / 2
+
+                counter += 1
+
+                time.sleep(0.70)
+
+            if counter >= 5 and counter <= 10:
+                clock_wise_rotate -= pi / 2
+                counter_clock_wise_rotate += pi / 2
+
+                red_box.rotate = transform.Rotate(
+                    counter_clock_wise_rotate, alignment.center
+                )
+                blue_box.rotate = transform.Rotate(clock_wise_rotate, alignment.center)
+
+                red_box.update()
+                blue_box.update()
+
+                counter += 1
+
+                time.sleep(0.70)
+
+            if counter > 10:
+                counter = 0
 
     page.add(
-        ft.Rive(
-            "lab.riv",
-            placeholder=ft.ProgressBar(),
-            width=600,
-            height=400,
-            alignment=ft.alignment.center,
-        ),
-        text_custom,password,
-        ft.ElevatedButton("Confirmar", on_click=open_dlg), 
-        ft.FloatingActionButton(icon=ft.icons.HELP, on_click=open_dlg_modal),
+        Card(
+            width=408,
+            height=612,
+            elevation=15,
+            
+            content=Container(
+                
+                image_src="asset\laboratorio.jpg",
+                image_fit=ImageFit.COVER,
+                image_opacity=.2,
+                bgcolor="black",
+                
+                border_radius=6,
+                content=Column(
+                    horizontal_alignment=CrossAxisAlignment.CENTER,
+                    controls=[
+                        
+                        Divider(height=60, color="transparent"),
+                        Stack(
+                            controls=[
+                                AnimatedBox("#e9665a", None, 0),
+                                AnimatedBox("#7df6dd", None, pi / 4),
+                            ]
+                        ),
+                        
+                        Divider(height=60, color="transparent"),
+                        Column(
+                            alignment=MainAxisAlignment.CENTER,
+                            horizontal_alignment=CrossAxisAlignment.CENTER,
+                            spacing=5,
+                            controls=[
+                                
+                                Text("VaultChest", size=30, weight="bold"),
+                                Text(
+                                    "VaultChest securely stores laboratory passwords.",
+                                    size=13,
+                                    weight="bold",
+                                ),
+                                
+                            ],
+                        ),
+                        Divider(height=1, color="transparent"),
+                        password,
+                        Divider(height=1, color="transparent"),
+                        ElevatedButton("Confirmar", on_click=open_dlg), 
+                        Divider(height=5, color="transparent"),
+                        FloatingActionButton(icon=icons.HELP, on_click=open_dlg_modal,),
+                        Divider(height=8, color="transparent"),
+                        Text(
+                            "Colegio Rio de la Plata || Escape Room 2024",
+                            size=10,
+                            color="white54",
+                        ),
+                    ],
+                ),
+            ),
         )
-    
-    
+    )
+    page.update()
+    animate_boxes()
 
-ft.app(target=main)
+
+if __name__ == "__main__":
+    flet.app(target=main)
